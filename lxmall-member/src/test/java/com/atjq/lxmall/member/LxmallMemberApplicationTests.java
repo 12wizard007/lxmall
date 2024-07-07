db6e4aac637576ff15692bd0e190f9743f2065b7
9595kmrd21 <1797008194@qq.com>
2024-07-07 11:23:21 +0800

    项目初始结构创建

---
+++ lxmall-member/src/test/java/com/atjq/lxmall/member/LxmallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atjq.lxmall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxmallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
